c1584e7001a46bab784a6306155326051502e065
Dexter <a.esselink@gmail.com>
2021-06-26 18:31:31 +0200

    Initial setup, talking to IPFS

---
+++ README.md
@@ -0,0 +1,14 @@
+# BENDERFACToRY vault
+
+- Powered by [svelte-kit])(https://kit.svelte.dev/) and [IPFS](https://ipfs.io)
+- Queries the vault on IPFS for all the data
+- Renders a nice shiny UI
+- Publishes the shiny UI on IPFS
+
+## Running locally for development
+
+- Install [nodejs](https://nodejs.org/en/download/)
+- Run `npm install`
+- Run `npm dev`
+- Open http://localhost:3000 in the browser
+
---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const reqNS = await fetch('https://ipfs.io/api/v0/name/resolve?arg=mm.em32.net')
+    // const { Path } = await reqNS.json()
+    const Path = 'QmZZ1gFyrrHXbDeiUUhaC9QvYYMo5xq12m4AAYFDwKDDyA'
+
+    const reqMetadata = await fetch(`https://ipfs.io/api/v0/cat?arg=${Path}/metadata.json`)
+    const seasons = await reqMetadata.json()
+
+    return {
+      props: {
+        seasons
+      }
+    }
+  }
+</script>
+
+<script>
+  export let seasons = []
+</script>
+
+
+<h1>Welcome to the BENDERFACToRY Vault</h1>
+  
+<ul>
+  {#each seasons as season}
+    <li><a href='/{season.path}'>{season.name}</a></li>
+  {/each}
+</ul>
+
+<pre>{JSON.stringify(seasons, null, 2)}</pre>
+<!-- <pre>{data}</pre> -->
